eb48961af1909c33c809a5c0448ccab624fe1033
Iran Lourenço <81264449+iranilsj@users.noreply.github.com>
2024-04-24 10:46:23 -0300

    Incluindo funcoes  de validação de Email e Senha

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,18 +1,30 @@
 // VALIDACAO DA PAGINA DE LOGIN
 
 function validateFields(){
+    const emailValid = isEmailValid();
+    document.getElementById('recover-password-button').disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById('login-button').disabled = !emailValid || !passwordValid
+}
+function isEmailValid(){
     const email = document.getElementById("email").value;
     if(!email){
-        document.getElementById('recover-password-button').disable = true;
-    } else if (validateEmail(email)){
-        document.getElementById('recover-password-button').disable = false;
-    } else {
-        document.getElementById('recover-password-button').disable = true;
+        return false;
     }
-
-    function
+    return validateEmail(email);
+}
+function isPasswordValid(){
+    const password =  document.getElementById('password').value
+    if (!password){
+        return false; 
+    }
+    return true;
+}
+function validateEmail(email){
+    return /\S+@\S+\.\S+/.test(email);
+}
     // pegar o valor do campo de e mail
     // verificar se o email nao é vazio e se é válido
     //se verdadeiro, habilitar o botao de recuperar senha 
     // se falto,desabilitar o botao de recuperar senha 
-}
